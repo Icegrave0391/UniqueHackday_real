c7b3fb24dc0ca6b22e68f6345e6afea0c12eb214
LeeG4ng <LeeG4ng@qq.com>
2018-06-03 11:19:31 +0800

    push

--- UniqueHackday_real/Controller/SkillController.m
+++ UniqueHackday_real/Controller/SkillController.m
@@ -8,9 +8,12 @@
 
 #import "SkillController.h"
 #import "TopView.h"
+#import "HeaderCell.h"
+#import "Tab2.h"
+#import "ArticleCell.h"
 #import "UIResponder+FirstResponder.h"
 
-@interface SkillController ()
+@interface SkillController () <UITableViewDelegate, UITableViewDataSource>
 
 @end
 
@@ -26,7 +29,11 @@
     [self.view addSubview:topView];
     [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard)]];
     
-    UITable
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 68, self.view.frame.size.width, self.view.frame.size.height-48) style:UITableViewStylePlain];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [self.view addSubview:tableView];
     // Do any additional setup after loading the view.
 }
 
@@ -35,6 +42,59 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+#pragma mark - datasource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 7;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    if(indexPath.row == 1|| indexPath.row == 3) return 150;
+    if(indexPath.row > 4) return 116;
+    return 90;
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        Tab2 *cell = [[Tab2 alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"hori"];
+        return cell;
+    }
+    else if (indexPath.row == 1) {
+        static NSString *ID = @"article";
+        Tab2 *article = [tableView dequeueReusableCellWithIdentifier:ID];
+        if (!article) {
+            article = [[Tab2 alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+        }
+        return article;
+    }
+    else if (indexPath.row == 2) {
+        static NSString *ID = @"article";
+        Tab2 *article = [tableView dequeueReusableCellWithIdentifier:ID];
+        if (!article) {
+            article = [[Tab2 alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+        }
+        return article;
+    }
+    else if (indexPath.row == 3) {
+        Tab2 *cell = [[Tab2 alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"hori"];
+        return cell;
+    }
+    else {
+        static NSString *ID = @"article";
+        Tab2 *article = [tableView dequeueReusableCellWithIdentifier:ID];
+        if (!article) {
+            article = [[Tab2 alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
+        }
+        return article;
+    }
+    
+}
 //键盘收起
 - (void)hideKeyboard {
     NSLog(@"tap gesture");
